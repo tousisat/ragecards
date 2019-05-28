6d327ce406220742972d00f4b0720539d71ec33d
Raphael <raphaelbridi@yahoo.com>
2019-05-28 18:44:59 -0400

    fix routing issue

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const store = createStore(
 
 const app = (
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </Provider>
