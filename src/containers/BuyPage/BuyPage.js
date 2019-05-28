e34001ead599ddd97bc00939b7eaa109fd5a06d3
Raphael <raphaelbridi@yahoo.com>
2019-05-28 00:31:21 -0400

    buy page

--- src/containers/BuyPage/BuyPage.js
+++ src/containers/BuyPage/BuyPage.js
@@ -1,7 +1,17 @@
 import React from "react";
+import UITitle from "./../../components/UI/Title/Title";
+import avatar from "../../assets/rainbow.png";
+import "./BuyPage.scss";
 
 const BuyPage = props => {
-  return <div>This is the Buy Page</div>;
+  return (
+    <div className="buy-page">
+      <div className="buy-page_title">
+        <UITitle img={avatar} title="Buy Now" subtitle="Make it yours!" />
+      </div>
+      blablbla
+    </div>
+  );
 };
 
 export default BuyPage;
